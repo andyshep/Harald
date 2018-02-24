93ad1e0b0b3d54c778e19fa076f37ce632cc8317
Andrew Shepard <shep.andy@gmail.com>
2018-02-24 10:42:42 -0500

    Initial import

---
+++ Harald/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Harald
+//
+//  Created by Andrew Shepard on 2/24/18.
+//  Copyright © 2018 Andrew Shepard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Harald/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Harald
+//
+//  Created by Andrew Shepard on 2/24/18.
+//  Copyright © 2018 Andrew Shepard. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
