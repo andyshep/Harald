6d4d5694c8fbdd39b226e85b65402335f203ec57
Andrew Shepard <shep.andy@gmail.com>
2018-02-25 13:41:16 -0500

    Show found peripherals

--- Harald/ViewController.swift
+++ Harald/ViewController.swift
@@ -7,13 +7,42 @@
 //
 
 import Cocoa
+import CoreBluetooth
+import RxSwift
+import RxCocoa
+
+extension ObservableType where E: Sequence, E.Iterator.Element: Equatable {
+    func distinctUntilChanged() -> Observable<E> {
+        return distinctUntilChanged { (lhs, rhs) -> Bool in
+            return Array(lhs) == Array(rhs)
+        }
+    }
+}
 
 class ViewController: NSViewController {
+    
+    @IBOutlet weak var tableView: NSTableView!
+    
+    private let bag = DisposeBag()
+    private let service = BluetoothService()
+    
+    private var objects: [CBPeripheral] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        _ = service.centralManager.state
 
-        // Do any additional setup after loading the view.
+        service.peripherials
+            .distinctUntilChanged()
+            .subscribe(onNext: { [unowned self] (periperhals) in
+                self.objects = periperhals
+                self.tableView.reloadData()
+            })
+            .disposed(by: bag)
     }
 
     override var representedObject: Any? {
@@ -21,7 +50,24 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
 }
 
+extension ViewController: NSTableViewDataSource {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return objects.count
+    }
+}
+
+extension ViewController: NSTableViewDelegate {
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let identifier = NSUserInterfaceItemIdentifier(rawValue: "NameCell")
+        if let cell = tableView.makeView(withIdentifier: identifier, owner: nil) as? NSTableCellView {
+            let object = objects[row]
+            
+            cell.textField?.stringValue = object.name ?? "No Name"
+            return cell
+        }
+        
+        return nil
+    }
+}
---
+++ Harald/WindowController.swift
@@ -0,0 +1,14 @@
+//
+//  WindowController.swift
+//  Harald
+//
+//  Created by Andrew Shepard on 2/25/18.
+//  Copyright © 2018 Andrew Shepard. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class WindowController: NSWindowController {
+    
+}
